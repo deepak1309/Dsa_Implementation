15add5c5a7cdd6f96676e6431d2c7d41dc008214
deepak1309 <sainideepak8861@gmail.com>
2023-09-28 11:28:48 +0530

    Changes

--- src/main/java/org/example/Graph/Graph.java
+++ src/main/java/org/example/Graph/Graph.java
@@ -32,7 +32,7 @@ public class Graph {
         }
         return false;
     }
-
+//test
     public boolean removeVertex(String vertex){
         if(adjList.get(vertex)==null) return false;
         for(String otherVertex:adjList.get(vertex)){
